b7dc178ae6a0502612aebadcd16e87e7037a5131
sleepyshowdy <hojo.miku@gmail.com>
2023-02-11 10:37:29 +1100

    sb: CatCard

--- commitlint.config.js
+++ commitlint.config.js
@@ -22,7 +22,8 @@ module.exports = {
     'subject-case': [
       2,
       'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+      // ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+      ['sentence-case', 'start-case', 'pascal-case'],
     ],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
