80002bf0caba17b96272aa6e82bb128db8f1579b
sleepyshowdy <hojo.miku@gmail.com>
2023-02-11 19:58:36 +1100

    feat: create results page and SearchResult component

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -1,18 +1,21 @@
 import Head from 'next/head';
 import Footer from '../../navigation/footer/Footer';
 import Header from '../../navigation/header/Header';
-
-export interface IPrimaryLayout {
-  children: any;
+export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<'div'> {
+  justify?: 'items-center' | 'items-start';
 }
 
-const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({
+  children,
+  justify = 'items-center',
+  ...divProps
+}) => {
   return (
     <>
       <Head>
         <title>NextJs Fullstack App Template</title>
       </Head>
-      <div className="min-h-screen flex flex-col items-center">
+      <div {...divProps} className={`min-h-screen flex flex-col ${justify}`}>
         <Header />
         <main>{children}</main>
         <div className="m-auto" />
