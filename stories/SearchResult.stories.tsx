aff3e1f5e71aca3e83cc240c555d37d838bde1c7
sleepyshowdy <hojo.miku@gmail.com>
2023-02-11 19:58:36 +1100

    feat: create results page and SearchResult component

---
+++ stories/SearchResult.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import SearchResult, {
+  ISearchResult,
+} from '../components/utility/search-result/SearchResult';
+import { mockSearchResultProps } from '../components/utility/search-result/SearchResult.mocks';
+
+export default {
+  title: 'utility/SearchResult',
+  component: SearchResult,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof SearchResult>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof SearchResult> = (args) => (
+  <SearchResult {...args} />
+);
+
+export const Base = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+Base.args = {
+  ...mockSearchResultProps.base,
+} as ISearchResult;
