734ac70bef8a1619fb0f1ad4e01e923cd16e199a
sleepyshowdy <hojo.miku@gmail.com>
2023-02-11 15:55:26 +1100

    feat: create Search component

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 export interface ISearch {
   sampleTextProp: string;
 }
 
 const Search: React.FC<ISearch> = ({ sampleTextProp }) => {
+  const [searchTerm, setSearchTerm] = useState<string>();
   return (
-    <form className="flex flex-col items-center gap-y-5">
+    <form
+      className="flex flex-col items-center gap-y-5"
+      onSubmit={(e) => {
+        e.preventDefault();
+        alert(`Action requested. Search for term: ${searchTerm}`);
+      }}
+    >
       <input
         type="text"
         className="rounded-full border-2 w-5/6 sm:w-96 h-12 px-3"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
+
       <div className="space-x-3">
         <button
           type="submit"
